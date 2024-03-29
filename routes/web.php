34453032623855459311dd4940bd561ed7e69bdd
debb4 <debbyazzahra05@gmail.com>
2024-03-29 23:25:52 +0700

    tampilan login_register

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/login', function () {
+    return view('login_register.login');
 });
